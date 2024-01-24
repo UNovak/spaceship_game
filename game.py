269acf52192b704d36dd6a26d46b9200626140c4
UNovak <urbanwritescode@gmail.com>
2024-01-24 18:46:25 +0100

    Added .gitignore player follows mouse

--- game.py
+++ game.py
@@ -7,20 +7,32 @@ WIDTH, HEIGHT = 800, 400
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aliens game")
 
-def draw(player):
+# variables
+WHITE = (255, 255, 255)
+
+
+def draw(player, mouse_x, mouse_y):
+    WIN.fill(WHITE)
+    player.update_angle(mouse_x, mouse_y)
     player.draw(WIN)
     pygame.display.update()
 
+
 def main():
     run = True
-    player = Player(WIDTH / 2, HEIGHT/2)
+    player = Player(WIDTH / 2, HEIGHT / 2)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
-        draw(player)
+        # Get mouse position
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        # update player
+        draw(player, mouse_x, mouse_y)
+
 
 if __name__ == "__main__":
     main()
--- objects.py
+++ objects.py
@@ -1,7 +1,8 @@
 import pygame as pg
+import math
 
-PLAYER_WIDTH = 50
-PLAYER_HEIGHT = 33
+PLAYER_WIDTH = 55
+PLAYER_HEIGHT = 55
 
 
 class Player:
@@ -9,7 +10,16 @@ class Player:
         self.x = x
         self.y = y
         self.img = pg.transform.scale(pg.image.load('Assets/Images/player_ship.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+        self.angle = 0
+
+    def update_angle(self, mouse_x, mouse_y):
+        # Calculate the angle in radians
+        angle_rad = math.atan2(mouse_y - self.y, mouse_x - self.x)
+        # Convert the angle to degrees and adjust for the Pygame coordinate system
+        self.angle = math.degrees(angle_rad) + 90
 
     def draw(self, window):
-        window.blit(self.img, ((self.x-(PLAYER_WIDTH/2)), (self.y-(PLAYER_HEIGHT/2))))
+        rotated_img = pg.transform.rotate(self.img, -self.angle)  # Rotate the image
+        rect = rotated_img.get_rect(center=(self.x, self.y))
+        window.blit(rotated_img, rect.topleft)
 
